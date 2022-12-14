ccc53cdc569861f1eb66c4aa3eacfed4b2105702
jorbian <5533@holbertonstudents.com>
2022-12-13 18:43:17 -0600

    Tried again.

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -17,8 +17,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	new_node->n = value;
 	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
+	new_node->left = parent->left;
+	new_node->right = parent->right;
 
 	return (new_node);
 }

78f73e6de1be7a644a8bdd3cb4e002a4ac43f8af
jorbian <5533@holbertonstudents.com>
2022-12-13 18:45:39 -0600

    Tried again.

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,7 +9,7 @@
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = NULL;
+	binary_tree_t *new_node;
 
 	new_node = malloc(sizeof(binary_tree_t));
 	if (parent == NULL)
@@ -17,8 +17,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	new_node->n = value;
 	new_node->parent = parent;
-	new_node->left = parent->left;
-	new_node->right = parent->right;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
 	return (new_node);
 }

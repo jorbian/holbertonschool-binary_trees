0a4f21281cdc537d73c5e0792cb0f24de558f698
jorbian <5533@holbertonstudents.com>
2022-12-13 19:15:53 -0600

    Another attempt to redo tasks 1 and 2.

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -14,18 +14,15 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	if (parent->right != NULL)
-		parent = parent->right;
-
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
-
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+	}
 	parent->right = new_node;
 
 	return (new_node);

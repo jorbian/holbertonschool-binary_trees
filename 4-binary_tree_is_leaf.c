4854a0618b805e2bf16db2b6c46838333bc0889b
jorbian <60049930+jorbian@users.noreply.github.com>
2022-12-19 14:36:08 -0600

    First attempt at fifth task.

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,1 +1,12 @@
 #include "binary_trees.h"
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+    if (node == NULL)
+        return (NULL);
+
+    if ((node->right != NULL) || (node->left != NULL))
+        return (1);
+
+    return (0);
+}

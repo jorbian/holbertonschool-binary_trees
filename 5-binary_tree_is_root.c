1633c13b679bd8cdaa4aa998052cf00ea9143822
jorbian <60049930+jorbian@users.noreply.github.com>
2022-12-19 14:55:19 -0600

    Inital upload of sixth task.

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,6 +1,18 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_root - determines whether or not a given node is a root
+ * @node: the node to be tested
+ *
+ * Return: '0' if it's not a leaf, '1' if it is.
+ */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    
+	if (node == NULL)
+		return (0);
+
+	if ((node->right != NULL) && (node->left != NULL))
+		return (1);
+
+	return (0);
 }

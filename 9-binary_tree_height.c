1ca28126ded8a16d04bf1d1e07691265223a58ca
jorbian <60049930+jorbian@users.noreply.github.com>
2022-12-19 16:01:41 -0600

    adsf

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -4,7 +4,7 @@
  * binary_tree_height - Measures the height of a binary tree.
  * @tree: Pointer to the root node to measure the height.
  *
- * Return: If tree is NULL, your function must return 0, else return height.
+ * Return: Tree is NULL, function returns 0, else returns the height.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {

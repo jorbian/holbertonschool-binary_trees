9ecccd76f75c98c975e1ad1fd3ff2f97178bc3dc
jorbian <60049930+jorbian@users.noreply.github.com>
2022-12-19 14:46:36 -0600

    Fixes to betty and other things.

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,12 +1,18 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_leaf - determines whether or not a given node is a leaf
+ * @node: the node to be tested
+ *
+ * Return: '0' if it's not a leaf, '1' if it is.
+ */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node == NULL)
-        return (NULL);
+	if (node == NULL)
+		return (0);
 
-    if ((node->right != NULL) || (node->left != NULL))
-        return (1);
+	if ((node->right != NULL) || (node->left != NULL))
+		return (0);
 
-    return (0);
+	return (1);
 }

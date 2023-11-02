e6e37ffead431028748bd8d12eb42031896ccff5
jorbian <5533@holbertonstudents.com>
2023-11-01 23:16:22 -0500

    Just pushing something for now.

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -1,20 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * find_height - Measures the height of a binary tree.
+ * binary_tree_height - Measures the height of a binary tree.
  * @tree: Pointer to the root node to measure the height.
  *
  * Return: Tree is NULL, function returns 0, else returns the height.
  */
-static size_t find_height(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t counters[2];
-
+																										
 	if (tree == NULL)
 		return (0);
 
-	counters[LEFT] = tree->left ? 1 + find_height(tree->left) : 0;
-	counters[RIGHT] = tree->right ? 1 + find_height(tree->right) : 0;
+	counters[LEFT] = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	counters[RIGHT] = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 	return (
 		(counters[LEFT] > counters[RIGHT]) ? counters[LEFT] : counters[RIGHT]
@@ -22,17 +22,26 @@ static size_t find_height(const binary_tree_t *tree)
 }
 
 /**
- * find_balance - Measure balance factor of binary tree.
- * @tree: A pointer to the root node of the tree to measure the balance factor.
+ * binary_tree_is_avl - checks if a binary tree is a valid AVL Tree
+ * @tree: Pointer to the root node to measure the height.
  *
- * Return: If tree is NULL, returns 0, else returns balance factor.
+ * Return: 1 if tree is a valid AVL Tree, and 0 otherwise
  */
-int find_balance(const binary_tree_t *tree)
+int binary_tree_is_avl(const binary_tree_t *tree)
 {
+	int counters[2];
+
 	if (tree == NULL)
 		return (0);
 
-	return (
-		find_height(tree->left) - find_height(tree->right)
-	);
+	counters[LEFT] = binary_tree_height(tree->left);
+	counters[RIGHT] = binary_tree_height(tree->right);
+
+	if(
+		(abs(counters[LEFT] - counters[RIGHT]) <= 1) &&
+		(binary_tree_is_avl(tree->left) && binary_tree_is_avl(tree->right))
+	)
+		return (1);
+
+   return (0);
 }

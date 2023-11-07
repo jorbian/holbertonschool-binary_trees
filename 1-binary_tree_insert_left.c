43efb2629449bdce8caab138a02089a3d4273bbe
Jonathan Boller <5533@holbertonstudents.com>
2023-11-07 12:53:23 -0600

    Finally cracked that single function left right thing.

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,14 +1,31 @@
 #include "binary_trees.h"
 
+/**
+ * get_left - retrieve the left hand child node
+ * @node: pointer to the node that's being looked at
+ *
+ * Return: A double pointer to the node on the side needed
+*/
 static binary_tree_t **get_left(binary_tree_t *node)
 {
 	return (&(node->left));
 }
 
-
-static binary_tree_t *insert_node(binary_tree_t *parent, int value, binary_tree_t **(*get_side)(binary_tree_t *))
+/**
+ * insert_node - insert a node on the left or the right slot of the parent
+ * @parent: the parent node being added to
+ * @value: the value that will be stored in the new node.
+ * @get_side: function pointer to the function that will dereference
+ * needed side on parent node.
+ *
+ * Return: Pointer to the newly added node.
+*/
+static binary_tree_t *insert_node(
+	binary_tree_t *parent, int value,
+	binary_tree_t **(*get_side)(binary_tree_t *)
+)
 {
-	binary_tree_t *new;
+	binary_tree_t *new, *temp;
 	binary_tree_t **parent_side;
 
 	if (parent == NULL)
@@ -19,16 +36,23 @@ static binary_tree_t *insert_node(binary_tree_t *parent, int value, binary_tree_
 		return (NULL);
 
 	parent_side = get_side(parent);
+	temp = *parent_side;
 
-	if (*parent_side != NULL)
-	{
-		(*parent_side)->parent = new;
-	}
-	parent_side = &new;
+	if (temp != NULL)
+		temp->parent = new;
+
+	*parent_side = new;
 
 	return (new);
 }
 
+/**
+ * binary_tree_insert_left - insert a new node on a binary tree on left
+ * @parent: parent node to the tree being added to
+ * @value: the value of the new node
+ *
+ * Return: A pointer to the new node that was just added.
+*/
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	if (parent == NULL)
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,29 +1,62 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - inserts a node in parents' right
- * @parent: parent of the node to be added
- * @value: numberic value
+ * get_right - retrieve the right hand child node
+ * @node: pointer to the node that's being looked at
  *
- * Return: pointer to the new node.
+ * Return: A double pointer to the node on the side needed
 */
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+static binary_tree_t **get_right(binary_tree_t *node)
 {
-	binary_tree_t *new_node;
+	return (&(node->right));
+}
+
+/**
+ * insert_node - insert a node on the right or the right slot of the parent
+ * @parent: the parent node being added to
+ * @value: the value that will be stored in the new node.
+ * @get_side: function pointer to the function that will dereference
+ * needed side on parent node.
+ *
+ * Return: Pointer to the newly added node.
+*/
+static binary_tree_t *insert_node(
+	binary_tree_t *parent, int value,
+	binary_tree_t **(*get_side)(binary_tree_t *)
+)
+{
+	binary_tree_t *new, *temp;
+	binary_tree_t **parent_side;
 
 	if (parent == NULL)
 		return (NULL);
 
-	new_node = binary_tree_node(parent, value);
-	if (new_node == NULL)
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
 		return (NULL);
 
-	if (parent->right != NULL)
-	{
-		new_node->right = parent->right;
-		parent->right->parent = new_node;
-	}
-	parent->right = new_node;
+	parent_side = get_side(parent);
+	temp = *parent_side;
 
-	return (new_node);
+	if (temp != NULL)
+		temp->parent = new;
+
+	*parent_side = new;
+
+	return (new);
+}
+
+/**
+ * binary_tree_insert_right - insert a new node on a binary tree on right
+ * @parent: parent node to the tree being added to
+ * @value: the value of the new node
+ *
+ * Return: A pointer to the new node that was just added.
+*/
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	if (parent == NULL)
+		return (NULL);
+
+	return (insert_node(parent, value, get_right));
 }

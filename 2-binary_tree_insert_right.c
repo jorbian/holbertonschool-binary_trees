97674623b98c4155b8b298da7e7fa145872cf63a
Jonathan Boller <5533@holbertonstudents.com>
2023-11-07 12:56:41 -0600

    Fix their stupid edge case hopefully...

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -55,7 +55,7 @@ static binary_tree_t *insert_node(
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	if (parent == NULL)
+	if (parent == NULL || parent->right != NULL)
 		return (NULL);
 
 	return (insert_node(parent, value, get_right));

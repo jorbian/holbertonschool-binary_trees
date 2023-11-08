a919fbaac6ce6156934cd5d92e4d5dfec27229ae
Jonathan Boller <5533@holbertonstudents.com>
2023-11-08 07:00:53 -0600

    See if this strategy works.

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -1,44 +1,88 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree.
+ * get_height - Measures the height of a binary tree.
  * @tree: Pointer to the root node to measure the height.
  *
  * Return: Tree is NULL, function returns 0, else returns the height.
  */
-static int binary_tree_height(const binary_tree_t *tree)
+static int get_height(const binary_tree_t *tree)
 {
-	size_t counters[2];
+	int counters[2];
 																										
 	if (tree == NULL)
 		return (0);
 
-	counters[LEFT] = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	counters[RIGHT] = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	counters[LEFT] = get_height(tree->left);
+	counters[RIGHT] = get_height(tree->right);
 
-	return (
-		(counters[LEFT] > counters[RIGHT]) ? counters[LEFT] : counters[RIGHT]
-	);
+	if (counters[LEFT] > counters[RIGHT])
+		return (counters[LEFT] + 1);
+	else
+		return (counters[RIGHT] + 1);
+}
+/**
+ * is_balanced - Check if the binary tree is balanced (not more than a
+ * difference of 1 in height)
+ * @tree: The tree or subtree to check
+ * Return: true if balanced, false if unbalanced
+*/
+static bool is_balanced(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (true);
+	else if (
+		abs(get_height(tree->left) - get_height(tree->right)) > 1
+	)
+		return (false);
+	else
+		return (
+			is_balanced(tree->left) &&
+			is_balanced(tree->right)
+		);
+}
+/**
+ * validate_avl - Checks if a binary tree is a valid AVL tree.
+ * @tree: A pointer to the root node of the tree to check.
+ * @lowest: The value of the smallest node visited thus far.
+ * @highest: The value of the largest node visited this far.
+ *
+ * Return: If the tree is a valid AVL tree, 1, otherwise, 0.
+ */
+static int validate_avl(const binary_tree_t *tree, int lowest, int highest)
+{
+	int height[2];
+
+	if (tree != NULL)
+	{
+		if (tree->n < lowest || tree->n > highest)
+			return (0);
+
+		height[LEFT] = get_height(tree->left);
+		height[RIGHT] = get_height(tree->right);
+
+		if (abs(height[LEFT] - height[RIGHT]) > 1)
+			return (0);
+		else
+			return (
+				validate_avl(tree->left, lowest, tree->n - 1) &&
+				validate_avl(tree->right, tree->n + 1, highest) &&
+				is_balanced(tree)
+			);
+	}
+	return (1);
 }
 
 /**
- * binary_tree_is_avl - checks if a binary tree is a valid AVL Tree
- * @tree: Pointer to the root node to measure the height.
+ * binary_tree_is_avl - Checks if a binary tree is a valid AVL tree.
+ * @tree: A pointer to the root node of the tree to check.
  *
- * Return: 1 if tree is a valid AVL Tree, and 0 otherwise
+ * Return: 1 if tree is a valid AVL tree, and 0 otherwise
  */
 bool binary_tree_is_avl(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (false);
+		return (0);
 
-	if(
-		(abs(
-			(binary_tree_height(tree->left)) - (binary_tree_height(tree->right)
-		)) <= 1) &&
-		(binary_tree_is_avl(tree->left) && binary_tree_is_avl(tree->right))
-	)
-		return (true);
-
-   return (false);
+	return (validate_avl(tree, INT_MIN, INT_MAX));
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,22 +1,34 @@
 #include "binary_trees.h"
 
 /**
+ * measure_branches - measure the subbranches of the tree
+ * @t: node of the root or sub-root of the tree
+ * @l: place to store the height down the left
+ * @r: place to store the height down the right
+*/
+static void measure_branches(const binary_tree_t *t, int *l, int *r)
+{
+	*l = binary_tree_height(t->left) + 1;
+	*r = binary_tree_height(t->right) + 1;
+}
+
+/**
  * binary_tree_height - Measures the height of a binary tree.
  * @tree: Pointer to the root node to measure the height.
  *
  * Return: Tree is NULL, function returns 0, else returns the height.
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+int binary_tree_height(const binary_tree_t *tree)
 {
-	size_t counters[2];
-																										
+	int counters[2];
+
 	if (tree == NULL)
 		return (0);
 
-	counters[LEFT] = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	counters[RIGHT] = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	measure_branches(tree, &counters[LEFT], &counters[RIGHT]);
 
-	return (
-		(counters[LEFT] > counters[RIGHT]) ? counters[LEFT] : counters[RIGHT]
-	);
+	if (counters[LEFT] > counters[RIGHT])
+		return (counters[LEFT]);
+	else
+		return (counters[RIGHT]);
 }
